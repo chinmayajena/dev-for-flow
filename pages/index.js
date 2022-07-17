dc2d62e176ccbb521035b5d58a7ff9b64a730891
chinmay <mail2chinmaya.jena@gmail.com>
2022-07-17 13:04:32 +0530

    added tailwind css

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1 style={{ position: "fixed", top: "30%", left: "25%" }}>
-      Welcome to D3. Go to{" "}
-      <Link href="/face">
-        <a style={{ color: "hotpink" }}>Face Smiley</a>
-      </Link>{" "}
-      page to see the d3 smiley!
-    </h1>
+    <div class="flex h-screen">
+      <div class="m-auto">
+        <h1 className="text-3xl font-bold underline">
+          Welcome to D3. Go to{" "}
+          <Link href="/face">
+            <a style={{ color: "hotpink" }}>Face Smiley</a>
+          </Link>{" "}
+          page to see the d3 smiley!
+        </h1>
+      </div>
+    </div>
   );
 }
